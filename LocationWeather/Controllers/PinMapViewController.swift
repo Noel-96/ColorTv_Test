2116c2d37fb7722f10908bd61b2003ab7ad7c428
Noel-96 <noelorion69@gmail.com>
2020-07-17 11:24:48 +0100

    added PinMapView Controller

---
+++ LocationWeather/Controllers/PinMapViewController.swift
@@ -0,0 +1,122 @@
+//
+//  PinMapViewController.swift
+//  LocationWeather
+//
+//  Created by Noel Obaseki on 17/07/2020.
+//
+
+import Foundation
+import UIKit
+import MapKit
+import CoreData
+import CoreLocation
+
+
+class PinMapViewController: UIViewController , MKMapViewDelegate , UIGestureRecognizerDelegate, CLLocationManagerDelegate {
+    
+    var dataController : ControlData!
+    @IBOutlet weak var mapView: MKMapView!
+    var pins: [Pin] = []
+    
+    let locationManager = CLLocationManager()
+    override func viewWillAppear(_ animated: Bool) {
+        addpin()
+        view.reloadInputViews()
+        
+        self.navigationController?.navigationBar.tintColor = UIColorFromRGB(colorCode: "4A9AA0", alpha: 1.0)
+       
+    }
+     override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+
+            if CLLocationManager.locationServicesEnabled() {
+                locationManager.delegate = self
+                locationManager.desiredAccuracy = kCLLocationAccuracyBest
+                locationManager.startUpdatingLocation()
+            }
+
+            mapView.delegate = self
+            mapView.mapType = .standard
+            mapView.isZoomEnabled = true
+            mapView.isScrollEnabled = true
+
+            if let coor = mapView.userLocation.location?.coordinate{
+                mapView.setCenter(coor, animated: true)
+            }
+        
+        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(tappedToAddPin))
+        gesture.delegate = self
+        mapView.addGestureRecognizer(gesture)
+        
+        let fetshRequest : NSFetchRequest<Pin> = Pin.fetchRequest()
+        if let result = try? dataController.viewContext.fetch(fetshRequest){
+            pins = result
+            mapView.removeAnnotations(mapView.annotations)
+            addpin()
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+
+        mapView.mapType = MKMapType.standard
+
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: locValue, span: span)
+        mapView.setRegion(region, animated: true)
+
+    }
+    
+    @objc func tappedToAddPin(gestureReconizer : UIGestureRecognizer){
+        if gestureReconizer.state == UIGestureRecognizer.State.began {
+            let location = gestureReconizer.location(in: mapView)
+            let coordinate = mapView.convert(location, toCoordinateFrom : mapView)
+            
+            let pin = Pin(context: dataController.viewContext)
+            pin.lat = coordinate.latitude.magnitude
+            pin.lon = coordinate.longitude.magnitude
+            do{
+                try dataController.viewContext.save()
+                
+            }catch{
+                print(error.localizedDescription)
+            }
+            pins.append(pin)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            mapView.addAnnotation(annotation)
+        }
+        
+    }
+    
+    
+    func addpin() {
+        mapView.removeAnnotations(mapView.annotations)
+        var annotations = [MKPointAnnotation]()
+        for pin in pins {
+            let coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(pin.lat), longitude:  CLLocationDegrees(pin.lon))
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotations.append(annotation)
+        }
+        mapView.addAnnotations(annotations)
+    }
+    
+    func UIColorFromRGB(colorCode: String, alpha: Float = 1.0) -> UIColor{
+        let scanner = Scanner(string:colorCode)
+        var color:UInt32 = 0;
+        scanner.scanHexInt32(&color)
+        
+        let mask = 0x000000FF
+        let r = CGFloat(Float(Int(color >> 16) & mask)/255.0)
+        let g = CGFloat(Float(Int(color >> 8) & mask)/255.0)
+        let b = CGFloat(Float(Int(color) & mask)/255.0)
+        
+        return UIColor(red: r, green: g, blue: b, alpha: CGFloat(alpha))
+    }
+}
+
